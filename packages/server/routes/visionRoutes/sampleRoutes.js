26ad9b6f8fcf70e9ecdddedc92cb9f12993d22be
anand <anandnandu@outlook.com>
2025-07-13 23:10:27 -0400

    json

--- packages/server/routes/visionRoutes/sampleRoutes.js
+++ packages/server/routes/visionRoutes/sampleRoutes.js
@@ -1,35 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../../lib/logger");
-const config = require("config");
-const axios = require("axios");
 const headers = require("../../middlewares/headers");
-const sampleData = require('../../database/Dropdowns/dropDowns.json')
-const stage = require('../../database/Environments/stage.json')
-const prod = require('../../database/Environments/prod.json')
-const latest = require('../../database/Environments/latest.json')
+
+const sampleData = require('../../database/Dropdowns/dropDowns.json');
+const stage = require('../../database/Environments/stage.json');
+const prod = require('../../database/Environments/prod.json');
+const latest = require('../../database/Environments/latest.json');
 
 router.get("/metaData", [headers], async (req, res, next) => {
-  const url = encodeURI(`https://dummyjson.com/todos`);
-  logger.debug(`calling anand utn ${url}`);
-   const combinedData = {
+  logger.debug("Returning combined local JSON data");
+
+  try {
+    const combinedData = {
       ...sampleData,
       ...stage,
       ...prod,
       ...latest
     };
-  axios({
-    method: "get",
-    url,
-    headers: { ...req.headers },
-    data: req.body,
-  })
-    .then((response) => {
-      const data = response.data;
-      //res.json(data);
-      res.json(combinedData);
-    })
-    .catch(next);
+
+    res.json(combinedData);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
